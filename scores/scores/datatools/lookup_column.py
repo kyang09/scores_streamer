c50f2b1b2222a9f608be3bafc4cd9f95ea95876d
Kevin Yang <kevinyang@Kevins-MacBook-Pro.local>
2018-11-30 10:42:27 -0800

    Added lookup_column module with LookupColumn class for Student and Exam to inherit.

---
+++ scores/scores/datatools/lookup_column.py
@@ -0,0 +1,13 @@
+class LookupColumn:
+	
+    def __init__(self, identifier):
+        self.id = identifier # String identifier of the lookup column.
+        self._db_indices = [] # Indices in the datastore related to the lookup.
+
+    def add_db_index(self, index):
+    	"""
+        Add an index to the _db_indices for datastore locations.
+
+        :param index: Index to a location in the datastore.
+        """
+    	self._db_indices.append(index)
