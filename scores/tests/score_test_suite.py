4a0e8ccbf19394ed6b139767eb9991aa6cdbe746
Kevin Yang <kevinyang@Kevins-MacBook-Pro.local>
2018-12-03 09:59:07 -0800

    Added tests for threading.

--- scores/tests/score_test_suite.py
+++ scores/tests/score_test_suite.py
@@ -2,6 +2,7 @@ import unittest
 
 # Test Modules
 import test_scores_api
+import test_score_stream_thread
 
 # Init test suite.
 loader = unittest.TestLoader()
@@ -9,7 +10,8 @@ suite  = unittest.TestSuite()
 
 # Add tests from test modules to the test suite.
 #suite.addTest(test_scores_api.TestScoresApi('test_exam_results_and_average'))
-suite.addTests(loader.loadTestsFromModule(test_scores_api))
+#suite.addTests(loader.loadTestsFromModule(test_scores_api))
+suite.addTests(loader.loadTestsFromModule(test_score_stream_thread))
 
 # Init a test runner.
 runner = unittest.TextTestRunner(verbosity=2)
---
+++ scores/tests/test_score_stream_thread.py
@@ -0,0 +1,53 @@
+import unittest
+import time
+from context import scores
+from scores.stream.score_stream_thread import ScoreStreamThread
+from scores.datatools.storage.memory_store import MemoryStore
+from scores.datatools.student import Student
+from scores.datatools.exam import Exam
+from scores.datatools.storage.memstoretools import student_tools
+from scores.datatools.storage.memstoretools import exam_tools
+
+class TestScoreStreamThread(unittest.TestCase):
+    """Tests for ScoreStreamThread."""
+
+    def setUp(self):
+        self.db = MemoryStore()
+        self.db.init([
+            (student_tools.ID_FIELD_NAME, Student),
+            (exam_tools.ID_FIELD_NAME, Exam)
+        ])
+        self.url = "http://live-test-scores.herokuapp.com/scores"
+        self.stream_thread = ScoreStreamThread(self.db, self.url)
+        self.max_wait_duration = 15 #  Duration is in seconds.
+
+    def test_thread_start(self):
+        self.assertFalse(self.stream_thread.isAlive())
+        self.stream_thread.start()
+        self.assertTrue(self.stream_thread.isAlive())
+
+    def test_thread_stop(self):
+        self.stream_thread.start()
+        self.assertTrue(self.stream_thread.isAlive())
+
+        start_time = time.time()
+        self.stream_thread.stop()
+        duration_too_long = False
+        while self.stream_thread.isAlive() and not duration_too_long:
+            if time.time() - start_time > self.max_wait_duration:
+                duration_too_long = True # If it takes too long, fail the test.
+        self.assertFalse(duration_too_long)
+        self.assertFalse(self.stream_thread.isAlive())
+
+    def test_thread_is_storing(self):
+        start_time = time.time()
+        self.stream_thread.start()
+        duration_too_long = False
+        while len(self.db._storage) == 0 and not duration_too_long:
+            if time.time() - start_time > self.max_wait_duration:
+                duration_too_long = True # If it takes too long, fail the test.
+        self.assertFalse(duration_too_long)
+        self.assertTrue(len(self.db._storage) > 0)
+
+    def tearDown(self):
+        self.stream_thread.stop()
